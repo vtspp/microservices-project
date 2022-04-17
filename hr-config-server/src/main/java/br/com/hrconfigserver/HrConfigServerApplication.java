45d48c0aacdc61c330d8d876e9640768783800a0
Victor Dos Santos Pinho <vtsppinho@gmail.com>
2022-04-17 10:29:02 -0300

    [#20220417] Configuração hr-config-server

---
+++ hr-config-server/src/main/java/br/com/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrConfigServerApplication.class, args);
+    }
+}
