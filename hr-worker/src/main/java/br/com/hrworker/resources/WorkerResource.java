2929fd9f404fdc3ab81dfb581179164009afbf74
Victor Dos Santos Pinho <vtsppinho@gmail.com>
2022-04-17 12:22:07 -0300

    [#20220417] Atualizar configurações em runtime

--- hr-worker/src/main/java/br/com/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import br.com.hrworker.repositories.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping (value = "/v1/workers")
 @RequiredArgsConstructor (onConstructor = @__(@Autowired))
