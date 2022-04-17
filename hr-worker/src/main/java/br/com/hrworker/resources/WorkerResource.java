5de068ec27328ad5c47c1c72ebfa5b98541fad84
Victor Dos Santos Pinho <vtsppinho@gmail.com>
2022-04-17 11:31:59 -0300

    [#20220416] Recuperar configurações do hr-config-server com profile de test

--- hr-worker/src/main/java/br/com/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/hrworker/resources/WorkerResource.java
@@ -4,6 +4,7 @@ import br.com.hrworker.entities.Worker;
 import br.com.hrworker.repositories.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,9 @@ public class WorkerResource {
 
     private final WorkerRepository repository;
 
+    @Value(value = "${test.config}")
+    private String configuration;
+
     @GetMapping
     public ResponseEntity<List<Worker>> findAll () {
         return ResponseEntity.ok(this.repository.findAll());
@@ -27,5 +31,11 @@ public class WorkerResource {
     @GetMapping (value = "/{id}")
     public ResponseEntity<Worker> findById (@PathVariable Long id) {
         return ResponseEntity.ok(this.repository.findById(id).get());
+    }
+
+    @GetMapping(value = "/configuration")
+    public ResponseEntity<Void> configuration () {
+        System.out.println(configuration);
+        return ResponseEntity.noContent().build();
     }
 }
